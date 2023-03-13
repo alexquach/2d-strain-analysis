be122e42c22017d24e1cf48cefbd12e66edc8186
Alex Quach <alexhq1000@gmail.com>
2023-03-13 14:49:34 -0400

    support fitting 6 lorentzians

--- lorentzian.py
+++ lorentzian.py
@@ -8,22 +8,22 @@ PL_parameters = ["offset", "x0_1", "a1", "gam1", "x0_2", "a2", "gam2"]
 def lorentzian(x, x0, a, gam):
     return a / (1 + (2 * (x - x0)/gam)**2 )
 
-def multi_lorentz_no_slope(x, off, x0_1, a1, gam1, x0_2, a2, gam2):
-        return off + lorentzian(x, x0_1, a1, gam1) + lorentzian(x, x0_2, a2, gam2)
-
-def multi_lorentz_with_slope(x, off, slope, x0_1, a1, gam1, x0_2, a2, gam2):
-        return off + slope * x + lorentzian(x, x0_1, a1, gam1) + lorentzian(x, x0_2, a2, gam2)
-
 def mse(popt, x, y, multi_lorentz):
     # off, x0_1, a1, gam1, x0_2, a2, gam2 = popt
     y_pred = [multi_lorentz(x_, *popt) for x_ in x]
     return ((y-y_pred)**2).mean()
 
-def fit_lorentzians_given_yData(yData, xData, startValues, bounds): 
+def two_lorentz_no_slope(x, off, x0_1, a1, gam1, x0_2, a2, gam2):
+        return off + lorentzian(x, x0_1, a1, gam1) + lorentzian(x, x0_2, a2, gam2)
+
+def two_lorentz_with_slope(x, off, slope, x0_1, a1, gam1, x0_2, a2, gam2):
+        return off + slope * x + lorentzian(x, x0_1, a1, gam1) + lorentzian(x, x0_2, a2, gam2)
+
+def fit_two_lorentzians_given_yData(yData, xData, startValues, bounds): 
     label_name = yData.name
     yData = yData / max(yData)
 
-    multi_lorentz = multi_lorentz_no_slope if len(startValues) == 7 else multi_lorentz_with_slope
+    multi_lorentz = two_lorentz_no_slope if len(startValues) == 7 else two_lorentz_with_slope
 
     try:
         popt, pcov = curve_fit( multi_lorentz, xData, yData.squeeze(), startValues, bounds=bounds, maxfev=100000 )   # Fit multiple Lorentzian functions to the data
@@ -31,6 +31,34 @@ def fit_lorentzians_given_yData(yData, xData, startValues, bounds):
         print(e)
         print(label_name)
         return [0, 0, 0, 0, 0, 0, 0, 420, 421] if len(startValues) == 7 else [0, 1, 0, 0, 0, 0, 0, 0, 420, 421]
+
+    # append MSE and R2 values to return result
+    y_pred = [multi_lorentz(x, *popt) for x in xData.values]
+    res_mse = mean_squared_error(yData.values, y_pred)
+    res_r2 = r2_score(yData.values, y_pred)
+
+    res = np.append(popt, [res_mse, res_r2])
+    return res
+
+
+def six_lorentz_no_slope(x, off, x0_1, a1, gam1, x0_2, a2, gam2, x0_3, a3, gam3, x0_4, a4, gam4, x0_5, a5, gam5, x0_6, a6, gam6):
+        return off + lorentzian(x, x0_1, a1, gam1) + lorentzian(x, x0_2, a2, gam2) + lorentzian(x, x0_3, a3, gam3) + lorentzian(x, x0_4, a4, gam4) + lorentzian(x, x0_5, a5, gam5) + lorentzian(x, x0_6, a6, gam6)
+
+def six_lorentz_with_slope(x, off, slope, x0_1, a1, gam1, x0_2, a2, gam2, x0_3, a3, gam3, x0_4, a4, gam4, x0_5, a5, gam5, x0_6, a6, gam6):
+        return off + slope * x + lorentzian(x, x0_1, a1, gam1) + lorentzian(x, x0_2, a2, gam2) + lorentzian(x, x0_3, a3, gam3) + lorentzian(x, x0_4, a4, gam4) + lorentzian(x, x0_5, a5, gam5) + lorentzian(x, x0_6, a6, gam6)
+
+def fit_six_lorentzians_given_yData(yData, xData, startValues, bounds): 
+    label_name = yData.name
+    yData = yData / max(yData)
+
+    multi_lorentz = six_lorentz_no_slope if len(startValues) == 1 + 3 * 6 else six_lorentz_with_slope
+
+    try:
+        popt, pcov = curve_fit( multi_lorentz, xData, yData.squeeze(), startValues, bounds=bounds, maxfev=100000 )   # Fit multiple Lorentzian functions to the data
+    except RuntimeError as e:
+        print(e)
+        print(label_name)
+        return [0] + [0, 0, 0] * 6 + [420, 421] if len(startValues) == 1 + 3 * 6 else [0, 1] + [0, 0, 0] * 6 + [420, 421]
 
     # append MSE and R2 values to return result
     y_pred = [multi_lorentz(x, *popt) for x in xData.values]
--- parallel_lorentz.py
+++ parallel_lorentz.py
@@ -2,19 +2,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from lorentzian import fit_lorentzians_given_yData
+from lorentzian import fit_two_lorentzians_given_yData, fit_six_lorentzians_given_yData
 
 import time
 start_time = time.time()
 
 is_raman = False
 with_slope = False
-type_process = "centroid" # "lorentz" or "centroid
+type_process = "lorentz6" # "lorentz" or "lorentz6" or "centroid" or "integrate"
 
 theoretical_raman_peak = 308
 # xlim = (theoretical_raman_peak-10, theoretical_raman_peak+10)
-# xlim = (700, 800)
-xlim = None
+xlim = (700, 800)
+pixel_lim = (18, 49, 85, 126)
+# xlim = None
+num_lorentzians = 6
+starting_peaks = [725, 730, 735, 740, 745, 750]
 
 # Raman
 # csv_path = 'multi_lossy_region15_raman_1mW_filter1_3.5K_grating2400_center250_exp.3s_x-150to150_y-150to150_100by100.csv'
@@ -33,6 +36,7 @@ if is_raman and with_slope:
     xData = df_filtered['Wavenumber']
     startValues = [0.1, 0, theoretical_raman_peak, 1, 1, theoretical_raman_peak+5, 1, 1]
     bounds = [-np.inf, -np.inf, 0, 0, 1, 0, 0, 1], [np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf]
+    # bounds = [-np.inf, -np.inf] + num_lorentzians * [0, 0, 1], [np.inf, np.inf] + num_lorentzians * [np.inf, np.inf, np.inf]
 elif is_raman:
     xData = df_filtered['Wavenumber']
     startValues = [0.1, theoretical_raman_peak, 1, 1, theoretical_raman_peak+5, 1, 1]
@@ -40,7 +44,8 @@ elif is_raman:
 else:
     xData = df_filtered["W"]
     startValues = [0.1, 725, 1, 30, 750, 1, 30]
-    bounds = [-np.inf, 0, 0, 5, 0, 0, 5], [np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf]
+    startValues = [0.1] + [ x for inner_list in [(starting_peak, 1, 5) for starting_peak in starting_peaks] for x in inner_list]
+    bounds = [-np.inf] + [700, 0, 1] * num_lorentzians, [np.inf] + [811, np.inf, np.inf] * num_lorentzians
 
 # print(df_filtered[(xData > xlim[0]) & (xData < xlim[1])])
 
@@ -59,23 +64,39 @@ else:
     df_iter = df_iter.drop(["W"], axis=1)
     # df_iter = df_iter.drop(["Unnamed: 0"], axis=1)
 
+def generate_list_of_column_numbers(xmin, xmax, ymin, ymax):
+  res_list = []
 
-def run_lorentz_peak_fit(name):
+  for x in range(xmin, xmax):
+    for y in range(ymin, ymax):
+      res_list.append(f"x{x}_y{y}")
+
+  return res_list
+if pixel_lim:
+    df_iter = df_iter[generate_list_of_column_numbers(*pixel_lim)]
+
+def run_lorentz_peak_task(name):
     print(f"Fitting {name}")
-    return fit_lorentzians_given_yData(df_iter[name], xData, startValues, bounds)
+    return fit_six_lorentzians_given_yData(df_iter[name], xData, startValues, bounds)
 
-def run_centroid_fit(name):
+def run_centroid_task(name):
     # print(f"Fitting {name}")
     return [centroid(df_iter[name], xData)]
+
+def run_integrate_task(name):
+    # print(f"Fitting {name}")
+    return [np.sum(df_iter[name])]
 
 # calculate centroid from 1d data:
 def centroid(yData, xData):
     return np.sum(yData * xData) / np.sum(yData)
 
-if type_process == "lorentz":
-    run_func = run_lorentz_peak_fit
+if type_process == "lorentz" or "lorentz6":
+    run_func = run_lorentz_peak_task
 elif type_process == "centroid":
-    run_func = run_centroid_fit
+    run_func = run_centroid_task
+elif type_process == "integrate":
+    run_func = run_integrate_task
 
 if __name__ == "__main__":
     from multiprocessing import Pool
@@ -90,7 +111,11 @@ if __name__ == "__main__":
     })
     print(f"Result:\n{result}")
     print("--- %s seconds ---" % (time.time() - start_time))
+
+    modifier = ""
+    if pixel_lim:
+        modifier = modifier + f"_x{pixel_lim[0]}to{pixel_lim[1]}_y{pixel_lim[2]}to{pixel_lim[3]}"
     if xlim:
-        result.to_csv(f'{csv_folder}{type_process}_{xlim[0]}to{xlim[1]}_{"wslope" if with_slope else "noslope"}_{csv_file}', index=False)
-    else:
-        result.to_csv(f'{csv_folder}{type_process}_{"wslope" if with_slope else "noslope"}_{csv_file}', index=False)
+        modifier = modifier + f"_{xlim[0]}to{xlim[1]}"
+    
+    result.to_csv(f'{csv_folder}{type_process}{modifier}_{"wslope" if with_slope else "noslope"}_{csv_file}', index=False)
